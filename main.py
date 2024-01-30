7b2d7f584696a67ccc09a489cbbf1cdc23e14bad
Geon <114966864+geon0430@users.noreply.github.com>
2024-01-31 00:26:33 +0900

    Update main.py

--- main.py
+++ main.py
@@ -27,7 +27,6 @@ async def predict(content: UploadFile = File(...), style: UploadFile = File(...)
     with open(style_path, "wb") as buffer:
         shutil.copyfileobj(style.file, buffer)
 
-    # AI 모델을 사용하여 이미지 합성
     makeArt(content_path, style_path, "output.jpg")
 
     return templates.TemplateResponse("result.html", {"request": {}, "content_path": content_path, "style_path": style_path, "output_path": "output.jpg"})
